303a39d5af0de7cce0e77b03deb803864d99f76a
Anthony Nguyen <anthony.c.nguyen@ucla.edu>
2016-11-07 23:53:58 -0800

    Revert "Refactor Code"
    This reverts commit 14dad36b13d188398fe99861ac6bf4cac7a630fc.

--- PythonApplication1/ConnectFour.py
+++ PythonApplication1/ConnectFour.py
@@ -1,91 +1,66 @@
 class ConnectFour():
     def __init__(self, height, width):
-        self.height = height
-        self.width = width
         self.board = []
-        self.columnCounts = [0]*width
         for i in range(width):
             self.board.append([0]*height)
-        print self.board
 
-        # False while game is being played; True when a player wins
         self.gameOver = False
+        self.currentPlayerTurn = True
         # True for player 1, False for player 2
-        self.currentPlayer = True
-        
-
-    def getPlayerNum(self):
-        if self.currentPlayer == True:
-            return '1'
-        if self.currentPlayer == False:
-            return '2'
-
-    def play(self):
-        while not self.gameOver:
-            for cidx, column in enumerate(self.board):
-                for ridx, row in enumerate(column):
-                    print self.board[cidx][ridx],
+        while True:
+            for i in range(len(self.board)):
+                for j in range (len(self.board[0])):
+                    print self.board[i][j],
                 print
 
-            print "Player" + self.getPlayerNum() + "'s Turn"
-            performedMove = self.getPlayerMove()
-            self.checkVictory(performedMove)
-            if(self.gameOver):
-                print "Player " + self.currentPlayer + " wins!"
+            print "Player" + str(self.currentPlayerTurn) + "'s Turn"
+            previousMove = self.getPlayerMove(self.currentPlayerTurn)
+            self.checkVictory(self.currentPlayerTurn, previousMove)
+            if(self.gameOver == True):
+                print "Player " + self.currentPlayerTurn + " wins!"
             else:
-                self.currentPlayer = not self.currentPlayer
+                self.currentPlayerTurn = not self.currentPlayerTurn
 
 
-    def getPlayerMove(self):
+    def getPlayerMove(self, playerNumber):
         validMove = False
-        column = -1
+        column = 0
         while not validMove:
-            column_str = raw_input("Please select a column: ")
-            if column_str.isdigit():
-                column_int = int(column)
-                validMove = self.checkValidMove(column_int)
-        return self.performPlayerMove(column_int)
+            column = int(raw_input("Which column would you like to place your token?"))
+            print column            
+            if column >=0 and column < len(self.board):
+                #column is okay, can place the token
+                validMove = True
+        #column contains a valid column
+        #actually perform the token insertion here
+        return self.performPlayerMove(column, playerNumber)
 
-    def checkValidMove(self, column):
-        if column >=0 and column < self.width:
-            #column is in the width of the board
-            if self.columnCounts[column] < self.height:                    
-                #column is not already filled
-                return True
-            else:
-                print "Column full"
-        return False
-        
-    def performPlayerMove(self, column):
-        nextEmptySpace = self.columnCounts[column]
-        playerToken = self.getToken()
+    def performPlayerMove(self, column, playerNumber):
+        playerToken = self.getToken(playerNumber)
         print "Performing Move"
-        curCol = self.board[column]
-        curCol[nextEmptySpace] = playerToken
-        self.columnCounts[column] += 1
-        return (column, nextEmptySpace)
+        for idx, row in enumerate(self.board[column]):
+            if idx == len(self.board[column])-1:
+                self.board[column][idx] = playerToken
+            print "row:"
+            print row
+            if row == 0:
+                continue
+            else:
+                self.board[column][row-1] = playerToken
+        return (column, row-1)        
 
     def checkMatch(self, x, y, playerToken, inARow = 0):
-        # Recursively checks to see if there are 4 of playerToken in a row
+        # fix off by one errors here
         if inARow == 4:
-            # There are 4 in a row
             return True
-        elif x <= 0 or x >= self.width - 1:
-            # Cannot look past the left or right edge of the board
-            return False
         #check left
         if (self.board[x-1][y] == playerToken):
             #keep searching left
             self.checkMatch(x-1, y, playerToken, inARow + 1)
         #check right
-        if(self.board[x+1][y] == playerToken):
+        elif(self.board[x+1][y] == playerToken):
             #keep searching right
             self.checkMatch(x+1, y, playerToken, inARow + 1)
-        
-        if y == 0:
-            # This is the bottom of a column, can't look below the bottom
-            return False        
-        
         #check below
         elif(self.board[x][y-1] == playerToken, inARow):
             #keep searching down
@@ -103,25 +78,26 @@ class ConnectFour():
             return False
         
 
-    def getToken(self):
-        if self.currentPlayer:
+    def getToken(self, playerNumber):
+        if playerNumber:
             playerToken = 1
         else:
             playerToken = -1
         return playerToken
     
-    def checkVictory(self, currentMove):
-        playerToken = self.getToken()
-        column = currentMove[0]
-        row = currentMove[1]
-        if self.checkMatch(column, row, playerToken, 0):
+    def checkVictory(self, playerNumber, previousMove):
+        playerToken = self.getToken(playerNumber)
+        x = previousMove[0]
+        y = previousMove[1]
+        if self.checkMatch(x, y, playerToken, 0):
             self.gameOver = True
         else:
             self.gameOver = False
+        
+        
 
 def main():
-    newGame = ConnectFour(7,3)
-    newGame.play()
+    ConnectFour(3,7)
 
 if __name__ == "__main__":
     main()
